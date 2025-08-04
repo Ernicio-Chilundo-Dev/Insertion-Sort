73aa1e8460b16c380bcd92caee9a21d54bb6cc19
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-08-04 14:00:41 +0200

    Implemented insertionSort algorithm in C: - Sorts an integer array in ascending order using insertion sort - Added printArray function for displaying arrays - Included main function with example input to demonstrate the algorithm

--- insertion.c
+++ insertion.c
@@ -18,7 +18,30 @@ void insertionSort(int arr[], int n)
             j--;
         }
 
-        arr[j + 1] = arr[j];
+        arr[j + 1] = key;
     }
 }
 
+// Function to print the array
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i  < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Original Array: ");
+    printArray(arr, n);
+
+    insertionSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+}
